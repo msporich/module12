220ca064895260483e7916e42f6abe3da55ca7b3
msporich <48558105+msporich@users.noreply.github.com>
2019-11-25 19:31:06 -0500

    updated

---
+++ jsonfiles.js
@@ -0,0 +1,57 @@
+/* Javascript Document */
+
+/***********************/
+/*    Module Twelve    */
+/*     JSON  FILES     */
+/***********************/
+
+//Step 1: Create Reference Variables
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+
+//Step 2: Create a Variable to Store Request URL
+let requestURL = "https://msporich.github.io/module12/iscream.json";
+
+//Step 3: Create a New XHR Object
+let request = new XMLHttpRequest();
+
+//Step 4: Open a New Request, Using the Open Method
+request.open("GET", requestURL);
+
+//Step 5: Set Up the Request to Accept JSON
+request.responseType = "json";
+
+//Step 6: Send the Request Using the Send Method
+request.send();
+
+//Step 7: Adding an Event Handler That Listens For Onload of the JSON File
+request.onload = function() {
+	
+	let iScreamInc = request.response;
+	console.log(iScreamInc);
+	populateHeader(iScreamInc);
+	topFlavours(IScreamInc);
+	
+}
+
+//Step 8: Define populateHeader method
+function populateHeader(jsonObj) {
+	
+	//grab the company name from the JSON object and then create a new element, adding text and appending to the header
+	let headerH1 = document.createElement("h1");
+	headerH1.textContent = jsonObj["companyName"];
+	header.appendChild(headerH1);
+	
+	//grab the company info and established date and add a new paragraph to your HTML that displays this info
+	let para = document.createElement("p");
+	para.textContent = jsonObj["headOffice"] + ", Established: " + jsonObj["established"];
+	header.appendChild(para);
+	
+}
+
+//Step 9: Define topFlavours method
+function topFlavours() {
+	
+	
+	
+}
